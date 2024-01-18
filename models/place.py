0f196aa13e40ffa80b1238b671610fe4f935a79e
TopBoyDrako <sinbadprince9@gmail.com>
2024-01-18 23:04:09 +0100

    update on base_model

--- models/place.py
+++ models/place.py
@@ -8,17 +8,3 @@ from os import getenv
 
 class Place(BaseModel, Base):
     """ Place class to store place information """
-    __tablename__ = 'places'
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        reviews = relationship("Review", backref="place",
-                               cascade="all, delete-orphan")
-    elif getenv('HBNB_TYPE_STORAGE') == 'file':
-        @property
-        def reviews(self):
-            """ Getter attribute for reviews in FileStorage """
-            reviews_list = []
-            for review in models.storage.all(Review).values():
-                if review.place_id == self.id:
-                    reviews_list.append(review)
-            return reviews_list
