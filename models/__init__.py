40100db9e48c7fa9a3dfacb7dea98c24875e35aa
kiplimoboor <kiplimoarapboor@gmail.com>
2024-01-19 19:23:45 +0300

    place_amenity table

--- models/__init__.py
+++ models/__init__.py
@@ -4,13 +4,6 @@
 from os import getenv
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 storage_type = getenv('HBNB_TYPE_STORAGE')
