9660d6aa18f610531b55a80a8f8f7c97b42a6535
kiplimoboor <kiplimoarapboor@gmail.com>
2024-01-19 12:58:50 +0300

    changes

--- models/review.py
+++ models/review.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from models.base_model import Base
 from models.base_model import BaseModel
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
+
+    __tablename__ = 'reviews'
     place_id = ""
     user_id = ""
-    text = ""
+    text:Mapped[str] = mapped_column(String(1024), nullable=False)
