c56fdc23c8d2921b9b6542c93a05ceb571928f75
TopBoyDrako <sinbadprince9@gmail.com>
2024-02-22 10:52:46 +0100

    2-do_deploy_web_static.py

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+This is a Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers, using the
+function do_deploy
+"""
+from fabric.api import *
+from datetime import datetime
+from os.path 
+
+
+env.hosts = ['54.296.35.48', '34.299.66.60']
+
+
+def do_deploy(archive_path):
+    """This distributes an archive to a web server"""
+
+     if exists(archive_path) is False:
+        return False
+    filename = archive_path.split('/')[-1]
+
+    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+
+    tmp = "/tmp/" + filename
+
+    try:
+        put(archive_path, "/tmp/")
+
+        run("mkdir -p {}/".format(no_tgz))
+
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("rm -rf {}/web_static".format(no_tgz))
+        run("rm -rf /data/web_static/current")
+
+        run("ln -s {}/ /data/web_static/current".format(no_tgz))
+
+        return True
+    except:
+        return False
